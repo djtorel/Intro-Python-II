5d8e6df2abe308c30c9a57fd6ddf3550e97fa12e
Dominic Torruellas <dom@domtorr.com>
2020-01-24 00:39:56 -0700

    convert input to lowercase

--- src/adv.py
+++ src/adv.py
@@ -129,7 +129,7 @@ def game_loop():
 
         while not input_results['proceed'] and not input_results['quit']:
             message = "\nThat was an invalid option\n"
-            player_input = input(f"{message}{input_option_string}")
+            player_input = str.lower(f"{message}{input_option_string}")
             input_results = process_input(player_input, player, options)
 
         if input_results['quit']:
